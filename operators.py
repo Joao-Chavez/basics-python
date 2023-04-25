e6018aa8e4319333f988d7fcc0b513b5df4976a0
Joao-Chavez <joaochavez10@gmail.com>
2023-04-24 20:23:29 -0600

    ADD: Operators

---
+++ operators.py
@@ -0,0 +1,21 @@
+addition = 10 + 5 #15
+substraction = 10 - 5 #5
+multiplication = 10 * 5 #50
+
+### COMPARISION OPERATORS
+age = 10 == 5 #False
+age = 10 != 5 #True
+age = 10 > 5 #True
+
+### LOGICAL OPERATORS
+age_boy = 10
+age_girl = 14
+
+age_boy > 8 and age_girl > 8 #True
+
+### ASSIGMENT OPERATORS
+age = 10 #Dará 10
+
+age += 5 #Dará 15
+age -= 5 #Dará 5
+age *= 5 #Dará 50
